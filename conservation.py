4462cc8cd698203e7abf18a425fb88ba2d9e74f4
B249274-2023 <email_not_provided>
2023-11-15 14:47:35 +0000

    added the practice script for the second step of the ICA – getting the conservation plots

--- conservation.py
+++ conservation.py
@@ -2,8 +2,12 @@
 
 
 # retrieving the sequences for the defined taxon and protein family
-subprocess.getoutput("esearch -db protein -query '" + protein + " [PROT] AND txid" + str(taxonID) + " [ORGN] NOT (predicted OR hypothetical)' | efetch -format fasta > ", protein + str(taxonID),".fasta")
+query_name = protein + "_txid" + str(taxonID)
 
+subprocess.getoutput("esearch -db protein -query '" + protein + " [PROT] AND txid" + str(taxonID) + " [Organism:exp] NOT (predicted OR hypothetical)' | efetch -format fasta > " + query_name + ".fasta")
 
-
-
+def plot_seq():
+    print("Creating conservation graphs, please wait.")
+    subprocess.call("clustalo -i " + query_name + ".fasta -o protein_alignment_" + query_name + ".msf --outfmt msf --threads 200 --force", shell = True )
+    subprocess.call("plotcon -sequences protein_alignment_" + query_name + ".msf -winsize 4 -graph png -goutfile plotcon", shell =True)
+    subprocess.call("gio open plotcon.1.png", shell = True)
