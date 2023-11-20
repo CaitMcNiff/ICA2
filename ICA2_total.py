825b9f621c742bf45889a24b15a52b78663f5968
B249274-2023 <email_not_provided>
2023-11-20 11:10:49 +0000

    final ICA2 script with all the comments and error trapping added.

--- ICA2_total.py
+++ ICA2_total.py
@@ -9,15 +9,16 @@ def dir():
         directory = input("\nwould you like to make a new directory for any ouput files? y/n\n")
         if directory == 'y':
             name = str(input("\nwhat would you like to call your directory?\n"))
-            os.mkdir(name) # make new directory based on the imputted name
+            os.makedirs(name, exist_ok=True) # make new directory based on the imputted name
             os.chdir(name) # change working directory to the new new directory
             print("you are now working in the new directory", name)
             return
         elif directory == 'n':
-            print("Okay no directory will be created and any output files will be created in your present directory")
+            print("Okay no directory will be created and any output files will be created in your present directory.\n")
             return
         else:
             print("\nInvalid input. \nPlease enter 'y' or 'n'.\n")
+
 
 # function to make sure taxonID input is an integer
 def check_integer(input_variable):
@@ -25,7 +26,7 @@ def check_integer(input_variable):
         int(input_variable)
         return True
     except ValueError:
-        print("The input is not an integer. Please check your input and try again.")
+        print("The input is not an integer. Please check your input and try again.\n")
         return False
 
 
@@ -36,12 +37,13 @@ def get_taxonID():
         if check_integer(user_input):
             return int(user_input)
 
-# function that asks the user to make sure they got the species that they wanted
+
+# function that checks the NCBI database to check the taxon exists and  asks the user to make sure they got the species that they wanted
 def tax_name():
     while True:
         taxonID = get_taxonID()
         print("You have entered:", taxonID)
-        # get the taxon name from the taxon ID
+        # get the taxon name from the taxon ID from the NCBI taxonomy database
         taxon_name = subprocess.getoutput("esearch -db taxonomy -query '" + str(taxonID) +  " [uid]' | efetch")
         # checking the user is happy with the input
         if taxon_name:
@@ -51,27 +53,31 @@ def tax_name():
                 print("\nGreat! Moving on.")
                 return taxon_name,taxonID
             elif user_input == 'n': # if they are not happy, then they get the chance to enter a new taxon ID
-                print("\nPlease try entering the taxon ID of interest again.")
+                print("\nPlease try entering the taxon ID of interest again.\n")
             else:
                 print("\nInvalid input. \nPlease enter 'y' or 'n'.\n")
         else:
             print("\nSpecies not found. \nPlease enter a valid Taxon ID.\n")
         continue
 
+
 # function that checks the inputted protein exists
 def protein_check():
     while True:
-        # defining the protein name of interest
-        protein = input("Please specify the name of the protein family of interest:\n")
-        # checking that the protein exists by seeing if there are results fetched from the NCBI library, counting the number of lines and saving this number as the variable prot_check
-        prot_check = int(subprocess.getoutput("esearch -db protein -query '" + protein +  " [PROT] NOT PARTIAL' | efetch -format uid | wc -l"))
-        # checking there is at least 1 row in file
-        if prot_check > 0:
-            print("Great,", prot_check, "sequences were found for" , protein, "in the NCBI database.")
-            return protein
-        else:
-            print("\nThere were no results for that protein in the NCBI database. Please make sure you entered your protein name correctly.")
-        continue
+        try:
+            # defining the protein name of interest
+            protein = input("Please specify the name of the protein family of interest:\n")
+            # checking that the protein exists by seeing if there are results fetched from the NCBI library, counting the number of lines and saving this number as the variable prot_check
+            prot_check = int(subprocess.getoutput("esearch -db protein -query '" + protein +  " [PROT] NOT PARTIAL' | efetch -format uid | wc -l"))
+            # checking there is at least 1 row in file
+            if prot_check > 0:
+                print("Great,", prot_check, "sequences were found for" , protein, "in the NCBI database.\n")
+                return protein
+            else:
+                print("\nThere were no results for that protein in the NCBI database. Please make sure you entered your protein name correctly.\n")
+        except Exception as e:
+            print("An error occurred: ",e)
+            print("Please check your input and try again.\n")
 
 
 # function that counts the number of sequences the query retrieves
@@ -85,27 +91,33 @@ def seq_count():
         line_count = accession_numbers.count("\n")
         # checking the number of sequences from the query
         if line_count < 1000:
-            print("\nYour query resulted in", line_count, "protein sequences.")
-            return
+            print("\nYour query of", protein, "and taxon ID", taxonID, " resulted in", line_count, "protein sequences.")
+            return line_count
         else:
-            q = input("\nYour query resulted in over 1000 protein sequences. That is quite a few sequences, are you sure you would like to carry on with these parameters? y/n\n").lower()
+            q = input("\nYour query resulted in over 1000 protein sequences. That is quite a few sequences, are you sure you would like to carry on with these parameters? (y/n)\n").lower()
             if q == 'y':
-                print("Okay, if youre sure.")
+                print("Okay, if youre sure.\n")
                 return
             elif q == 'n':
-                print("\nOkay, press 'control+D' on your keyboard to escape from the script and rerun the script with a new input taxon ID and/or protei.n\n")
+                print("\nOkay, press 'control+D' on your keyboard to escape from the script and rerun the script with a new input taxon ID and/or protein\n")
             else:
                 print("\nInvalid input. \nPlease enter 'y' or 'n'.\n")
 
-# function that aligns the protein sequences, plots a conservation plot based on the seqquences, and output the graph to the screen
+
+# function that aligns the protein sequences, plots a conservation plot based on the sequences, and output the graph to the screen
 def plot_seq():
-    print("Creating conservation graphs, please wait...")
     # using clustalo to align the sequences and convert the output to .msf
-    subprocess.call("clustalo -i " + query_name + ".fasta -o protein_alignment_" + query_name + ".msf --outfmt msf --threads 200 --force", shell = True )
+    if line_count == 1:
+        print("Skipping sequence alignment as there is only one sequence\n")
+    else:
+        subprocess.call("clustalo -i " + query_name + ".fasta -o protein_alignment_" + query_name + ".msf --outfmt msf --threads 200 --force", shell = True )
+        print("Protein alignment completed successfully!\n")
+    print("Creating conservation graphs, please wait...\n")
     # plotting the conservation plot of all the sequences that were retrieved from the NCBI database and saving it as a png
-    subprocess.call("plotcon -sequences protein_alignment_" + query_name + ".msf -winsize 4 -graph png -stdout > "+ query_name + "_plotcon.png", shell =True) ### TRY TO FIND OUT HOW TO CHANGE THE OUTPUT NAME FOR THE PLOTCON
+    subprocess.call("plotcon -sequences protein_alignment_" + query_name + ".msf -winsize 4 -graph png -stdout > "+ query_name + "_plotcon.png", shell = True)
     # bringing the conservation plot to the screen
-    subprocess.call("gio open plotcon.1.png", shell = True)
+    subprocess.call("eog "+query_name+ "_plotcon.png", shell = True)
+
 
 # function that splits the fasta file into individal sequence files with the new files being named after the accession numbers
 def split_seq(input_file, accn_file):
@@ -124,13 +136,48 @@ def split_seq(input_file, accn_file):
             accn = accn_line.strip()
             #mathcing the accession number from fasta file to accn.txt file to create output file name
             if accn == identifier:
-                output_filename = f"{accn}.fasta"
+                output_filename = f"sequences/{accn}.fasta"
                 with open(output_filename, 'w') as output_file:
                     output_file.write(f">{section}")
-                print(f"Match found! Created file: {output_filename}")
+                print(f"Created file: {output_filename}")
                 break
         else:
             print(f"No match found for {identifier}")
+
+
+# function that runs pepstats
+def run_pepstats():
+    while True:
+        # ask the user if they want to run pepstats
+        run_pepstats_response = input("Would you like to run pepstats (this gives varions statistics about the protein sequences) (y/n): \t").lower()
+        if run_pepstats_response == 'y':
+            # asking the user if they want individual sequence files
+            individual_files = input("\nDo you want individual sequence statistics files? (y/n): \nAnswering no will result in the sequence statistics being saved in the same file\n").lower()
+            # modifing the command based on user's choice
+            if individual_files == 'y':
+                os.makedirs("protein_stats", exist_ok=True)
+                # loop through every file in the sequences folder
+                for filename in os.listdir("sequences"):
+                    file_path = os.path.join("sequences", filename)
+                    print(f"Processing file: {filename}")
+                    # run pepstats for each individual sequence file
+                    subprocess.call("pepstats -sequence " + file_path + " -outfile protein_stats/" + filename + "_seq_stats.txt -auto Yes", shell = True)
+                print("\nThe pepstats output files were saved in a directory called 'protein_stats'.\n")
+                break # exit the loop
+            # if they chose no, the protein sequence statistics will all be outputted in one file
+            elif individual_files == 'n':
+                subprocess.call("pepstats -sequence " + query_name + ".fasta -outfile " + query_name + "_seq_stats.txt", shell = True)
+                print("pepstats executed successfully.\n")
+                break
+            else:
+                print("Invalid response. Please enter 'y' or 'n'. Try again.")
+        # if they don't want to run pepstats, it will be skipped
+        elif run_pepstats_response == 'n':
+            print("Okay. Skipping pepstats.")
+            break
+        else:
+            print("Invalid response. Please enter 'y' or 'n'. Try again.")
+
 
 ### End of function definition
 
@@ -143,20 +190,27 @@ dir()
 taxon_name, taxonID = tax_name()
 # defining the protein
 protein = protein_check()
+# checking if there is a space in the protein name and if there is, changing it to a dash
+if ' ' in protein:
+    protein = protein.replace(' ', '-')
 # checking the number of sequences for the query
-seq_count()
+line_count = seq_count()
 
 # retrieving the sequences for the defined taxon and protein family
+#defining the query name
 query_name = protein + "_txid" + str(taxonID)
-
 # getting the fasta sequences from the NCBI database from the query name
 subprocess.getoutput("esearch -db protein -query '" + protein + " [PROT] AND txid" + str(taxonID) + " [Organism:exp] NOT (predicted OR hypothetical)' | efetch -format fasta > " + query_name + ".fasta")
 
 #plotting conservation plot
 plot_seq()
 
+# making a directory for the the individual to go into
+os.makedirs("sequences", exist_ok=True)
+print("Separating fasta file into individual sequences") 
+
 # splititng the fasta file into individual sequence files
-split_seq(query_name,".fasta", "accn.txt")
+split_seq(query_name+".fasta", "accn.txt")
 
 # making a directory for the patmatmotifs output files to go into
 os.makedirs("motifs", exist_ok=True)
@@ -165,11 +219,11 @@ os.makedirs("motifs", exist_ok=True)
 with open("accn.txt", 'r') as accn_file:
     for accn in accn_file:
         accn = accn.strip()
-        # Use patmatmotifs to search for motifs in the chosen sequence file
-        subprocess.call("patmatmotifs -sequence " + accn + ".fasta -outfile " + accn + "_res.patmatmotifs -full -auto Yes", shell = True)
+        # use patmatmotifs to search for motifs in the chosen sequence file
+        subprocess.call("patmatmotifs -sequence sequences/" + accn + ".fasta -outfile motifs/" + accn + "_res.patmatmotifs -full -auto Yes", shell = True)
 
-#creating an output file for the sequences with motif hits
-output_file = 'motifs_hits.txt'
+# creating an output file for the sequences with motif hits
+output_file = 'motifs_hit.txt'
 dir_path = 'motifs/'
 
 # opening the output file in write mode
@@ -183,13 +237,20 @@ with open(output_file, 'w') as output:
             with open(file_path, 'r') as file:
                 for line in file:
                     line = line.strip()
+                    # checking if the line does not have a hit count of 0, if it doesn't the file name gets added to the new file called motif_hits.txt
                     if line.startswith('# HitCount:') and not line.startswith('# HitCount: 0'):
                         # If the condition is met, write the filename to the output file
                         output.write(filename + '\n')
-                        break  # Break out of the loop once a match is found
+                        break  # break out of the loop once a match is found
 
-# Print a message indicating the process is complete
+# printing a message indicating the process is complete
 print("Files with motif hits written to:", output_file)
-with open("motifs_hits.txt", 'r') as hits:
+with open("motifs_hit.txt", 'r') as hits:
     x = len(hits.readlines())
     print('Number of files that have one or more motif hits:', x)
+
+## ADDITIONAL ANALYSIS
+# pepstats analysis
+run_pepstats()
+
+print("Thank you! You have reached the end of this programme :)")
